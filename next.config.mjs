f52b658cabe7fa37f9b89424381df6e9db44881c
Konstantinov Nikita <n.konstantinov@g.nsu.ru>
2025-06-10 22:25:12 +0700

    add export output

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  // Uncomment and modify these lines if you're not using a custom domain
+  // basePath: '/your-repo-name',
+  // assetPrefix: '/your-repo-name/',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
